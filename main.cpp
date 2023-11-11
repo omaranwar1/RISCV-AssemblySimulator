2eb38134eb60d386fcf1ebfc1da598b06bb9fd0e
amalfouda <109471354+amalfouda@users.noreply.github.com>
2023-11-11 19:15:35 +0200

    last version of reading instruction

--- main.cpp
+++ main.cpp
@@ -12,15 +12,19 @@
 
 using namespace std;
 
-vector<string> R_type = {"add", "sub", "sll", "slt", "sltu", "xor", "srl", "sra", "or", "and"};
-vector<string> I_type = {"addi", "slti", "sltiu", "xori", "ori", "andi", "slli", "srli", "srai"};
+vector<string>R_type = {"add", "sub", "sll", "slt", "sltu", "xor", "srl", "sra", "or", "and"};
+vector<string>I_type = {"addi", "slti", "sltiu", "xori", "ori", "andi"};
 vector<string>I_type2={"lb","lh","lw","lbu","lhu"};
 vector<string>B_type={"beq","bne","blt","bge","bltu","bgeu"};
-vector<string>jump_link={"jal"};
-//vector<string>
+vector<string>S_type={"sw","sh","sb"};
+vector<string>jump_link={"jalr"};
+vector<string>jump2={"jal"};
+vector<string>shifiting={"slli","srli","srai"};
+vector<string>lower_upperImm={"lui","auipc"};
 class types {
 public:
-    string label, func, rs1, rs2, rd, imm, label_branching;
+    string label, func, rs1, rs2, rd, imm, label_branching,shamt,j_label;
+     
 };
 
 vector<types> read(vector<string> x) {
@@ -28,7 +32,7 @@ vector<types> read(vector<string> x) {
 
     for (auto& str : x) {
         stringstream ss(str);
-        string first_word, rd, rs1, rs2, imm, label_bran;
+        string first_word, rd, rs1, rs2, imm, label_bran, shamt,jumping_label;
         types t;
         
         // Extract first word from the string
@@ -92,27 +96,78 @@ vector<types> read(vector<string> x) {
             t.label_branching=label_bran;
             result.push_back(t);
         }
+        else if(find(S_type.begin(),S_type.end(),t.func)!=S_type.end())
+        {
+           getline(ss, rs1, ',');
+           getline(ss, imm, '('); //imm won't include any paranthesis
+           ss >> rs2;//rs1 will include the closing paranthesis
+           ss.ignore();
+
+    
+          t.rs1 = rs1.substr(rs1.find(' ') + 1);
+          t.imm = imm;
+          t.rs2 = rs2.substr(0, rs2.find(')'));
+
+          result.push_back(t);
+        }
         else if(find(jump_link.begin(), jump_link.end(),t.func)!=jump_link.end())
-               {
-                   getline(ss,rd,',');
-                   ss>>imm;
-                   t.rd=rd.substr(rd.find(' ')+ 1);
-                   t.imm=imm.substr(imm.find(' ')+1);
-                   result.push_back(t);
-               }
-               //I still didn't do (lui,auipc,jal,jalr,fence,ecall,ebreak)
+        {
+           getline(ss, rd, ',');
+           getline(ss, imm, '('); //imm won't include any paranthesis
+           ss >> rs1;//rs1 will include the closing paranthesis
+           ss.ignore();
+
+    
+          t.rd = rd.substr(rd.find(' ') + 1);
+          t.imm = imm;
+          t.rs1 = rs1.substr(0, rs1.find(')'));
+          result.push_back(t);
+        }
+        else if(find(jump2.begin(), jump2.end(),t.func)!=jump2.end())
+        {
+            getline(ss,rd,',');
+            ss>>jumping_label;
+            t.rd = rd.substr(rd.find(' ') + 1);
+            t.j_label=jumping_label;
+            result.push_back(t);
+            
+        }
+        else if(find(shifiting.begin(),shifiting.end(),t.func) != shifiting.end()) {
+            getline(ss, rd, ',');
+            getline(ss, rs1, ',');
+            ss >> shamt;
+
+            // Assign the destination, source, and immediate
+            t.rd = rd.substr(rd.find(' ') + 1);
+            t.rs1 = rs1.substr(rs1.find(')')+1 );
+            t.shamt = shamt;
+
+            result.push_back(t);
+        }
+        else if(find(lower_upperImm.begin(), lower_upperImm.end(),t.func)!=lower_upperImm.end())
+        {
+            getline(ss,rd,',');
+            ss>>imm;
+            t.rd = rd.substr(rd.find(' ') + 1);
+            t.imm=imm;
+            result.push_back(t);
+            
+        }
     }
 
     return result;
 }
+
+
 int main() {
-    vector<string> x = {"loop: sll x16,x20,x4", "t1: addi x5,x5,2", "t3: lw t0,4(t1)","bge s0,s3,exit1"};
+    
+    vector<string> x = {"loop: sll x16,x20,x4", "t1: addi x5,x5,2", "t3: lw t0,4(t1)","bge s0,s3,exit1","sw t0,6(t1)", "exist: jalr x0,4(ra)", "slli x10,x5,2","jal x0,func2","lui x0,0x12345"};
     vector<types> res = read(x);
 
     // Output the result for demonstration
     for (auto& t : res)
     {
-        cout<<"label:"<<t.label<<" , func:"<<t.func<<", rd:"<< t.rd<<", rs1:"<<t.rs1<<", rs2:"<<t.rs2<<", imm:"<<t.imm<<",branching_label:"<<t.label_branching<<endl;
+        cout<<"label:"<<t.label<<" , func:"<<t.func<<", rd:"<< t.rd<<", rs1:"<<t.rs1<<", rs2:"<<t.rs2<<", imm:"<<t.imm<<",branching_label:"<<t.label_branching<<", shamt:"<<t.shamt<<", jump:"<<t.j_label<<endl;
     }
        
 
